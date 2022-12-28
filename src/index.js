224d15dcfd39e024dfc48e6a18805385fe872ac4
euxsaud <saud.edgar@gmail.com>
2022-12-28 12:59:31 -0700

    Developing services section and configure splide-slide

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-import "materialize-css/dist/js/materialize.js";
+// import "alpinejs/dist/cdn.js";
+// import "materialize-css/dist/js/materialize.js";
+import "materialize-css/js/waves.js";
+import Splide from "@splidejs/splide";
 
-console.log("Hello World!");
+// Set fuction to run Splice Carousel, because Materialize Carousel is a piece of shit.
+function SplideSlide() {
+    const options = {
+        type: "loop",
+        perPage: 4,
+        arrows: false,
+    };
+
+    const splideOne = new Splide(".splide", options);
+
+    splideOne.mount();
+}
+
+// Set Materialize Carousel
+function mCarousel() {
+    const Elem = document.querySelector(".carousel");
+
+    // Init carousel
+    M.Carousel.init(Elem, {
+        dist: 0,
+        numVisible: 4,
+    });
+
+    // Autoplay carousel
+    const timeout = 5000;
+    let autoplay = true;
+    setInterval(() => {
+        if (autoplay) M.Carousel.getInstance(Elem).next();
+    }, timeout);
+    Elem.addEventListener("mouseover", () => {
+        autoplay = false;
+    });
+    Elem.addEventListener("mouseout", () => {
+        autoplay = true;
+    });
+}
+
+// Starting when document is ready
+document.addEventListener("DOMContentLoaded", () => {
+    SplideSlide();
+});
