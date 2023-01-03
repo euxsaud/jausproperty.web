3151f2f7a010e7ab3f56313baefda89b0235d93c
Edgar Saud <saud.edgar@gmail.com>
2023-01-02 23:57:58 -0600

    Finished visual part and responsive adaptation for any device

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ function SplideSlide() {
     const options = {
         perPage: 3,
         type: "loop",
-        arrows: false,
+        arrows: true,
         gap: 10,
         focus: "center",
         autoplay: true,
+        breakpoints: {
+            1024: {
+                perPage: 2,
+            },
+            768: {
+                perPage: 3,
+            },
+            500: {
+                perPage: 1,
+            },
+        },
     };
 
     const splideOne = new Splide(".splide", options);
@@ -19,31 +30,57 @@ function SplideSlide() {
     splideOne.mount();
 }
 
-// Set Materialize Carousel
-function mCarousel() {
-    const Elem = document.querySelector(".carousel");
+function NavControl() {
+    const Nav = document.querySelector("#nav-site");
+    const Button = document.createElement("button");
+    const Links = Nav.querySelector(".link-list");
 
-    // Init carousel
-    M.Carousel.init(Elem, {
-        dist: 0,
-        numVisible: 4,
-    });
+    // Create button trigger nav
+    Button.classList.add("btn-nav-trigger", "waves-effect");
+    Button.innerHTML = '<i class="material-icons">menu</i>';
+    Nav.appendChild(Button);
 
-    // Autoplay carousel
-    const timeout = 5000;
-    let autoplay = true;
-    setInterval(() => {
-        if (autoplay) M.Carousel.getInstance(Elem).next();
-    }, timeout);
-    Elem.addEventListener("mouseover", () => {
-        autoplay = false;
-    });
-    Elem.addEventListener("mouseout", () => {
-        autoplay = true;
+    // Create layout when the nav is open and semi-hide site.
+    const Layout = document.createElement("div");
+    Layout.classList.add("layout-nav");
+    document.body.appendChild(Layout);
+
+    // Function to hide or show nav depents their status.
+    const toggleNav = () => {
+        if (window.innerWidth < 600) {
+            if (Nav.classList.contains("hide-nav")) {
+                Nav.classList.remove("hide-nav");
+                Button.classList.add("active");
+                Layout.classList.add("active");
+                Button.querySelector("i").textContent = "clear";
+            } else {
+                Nav.classList.add("hide-nav");
+                Button.classList.remove("active");
+                Layout.classList.remove("active");
+                Button.querySelector("i").textContent = "menu";
+            }
+        }
+    };
+
+    Nav.classList.add("hide-nav");
+    Button.addEventListener("click", toggleNav);
+    Links.addEventListener("click", () => setTimeout(toggleNav, 200));
+
+    document.body.addEventListener("click", (event) => {
+        let Target = event.target;
+        let parents = [];
+
+        while (Target.parentNode && Target.parentNode.nodeName.toLowerCase() !== "body") {
+            parents.push(Target);
+            Target = Target.parentNode;
+        }
+
+        if (!parents.some((elem) => elem.id === "nav-site") && !Nav.classList.contains("hide-nav")) toggleNav();
     });
 }
 
 // Starting when document is ready
 document.addEventListener("DOMContentLoaded", () => {
     SplideSlide();
+    NavControl();
 });
